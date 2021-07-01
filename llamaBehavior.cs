10c76b0df1e1ece91350d18268993cb14b92160c
Chip <chiptalkscomputers@gmail.com>
2021-07-01 18:12:44 +0530

    Update llamaBehavior.cs

--- llamaBehavior.cs
+++ llamaBehavior.cs
@@ -11,6 +11,7 @@ public class llamaBehavior : MonoBehaviour
     bool isDrinking = false;
     private void Start()
     {
+        //Adjust positions to be on top of terrain
         herdCenter.y = Terrain.activeTerrain.SampleHeight(herdCenter);
         transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position), transform.position.z);
         randPos = herdCenter;
@@ -21,11 +22,12 @@ public class llamaBehavior : MonoBehaviour
     {
         if (!isDrinking)
         {
+            //Play walk animation
             if (anim.GetBool("isWalking"))
             {
                 transform.Translate(0, 0, 1f * Time.deltaTime);
                 transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) + 0.5f, transform.position.z);
-            }
+            }           
             if (Random.Range(1, 15000) < 100 && !anim.GetBool("isGrazing"))
             {
                 //Walk
@@ -49,6 +51,7 @@ public class llamaBehavior : MonoBehaviour
                 isDrinking = true;
                 StartCoroutine(DrinkWater());
             }
+            //Generate random position within herd
             else if (Random.Range(1, 20000) < 50)
             {
                 randPos = new Vector3(Random.Range(450, 550), Terrain.activeTerrain.SampleHeight(transform.position) + 0.5f, Random.Range(50, 150));
@@ -58,16 +61,20 @@ public class llamaBehavior : MonoBehaviour
 
     private void Group()
     {
+        //Turn back if too far away from herd
         if (Vector3.Distance(transform.position, herdCenter) > 25f)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(herdCenter - transform.position), 0.1f);
             transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) + 0.5f, transform.position.z);
             return;
         }
+        //Rotate towards randpos
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(randPos - transform.position), 0.1f);
     }
 
+    //Coroutine to drink water
     IEnumerator DrinkWater() {
+        //Turn towards waterpos
         Vector3 currentPos = transform.position;
         float elapsedTime = 0f;
         float waitTime = 10f;
@@ -78,7 +85,7 @@ public class llamaBehavior : MonoBehaviour
             //Debug.Log(elapsedTime + " " + waitTime);
             yield return null;
         }
-
+        //Move towards waterpos
         while (transform.position.x < waterpos.x && transform.position.z < waterpos.z)
         {
             if (!anim.GetBool("isWalking"))
@@ -90,15 +97,14 @@ public class llamaBehavior : MonoBehaviour
             transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) + 0.5f, transform.position.z);
             yield return null;
         }
-
-
+        //Drink
         anim.SetBool("isGrazing", true);
         anim.Play("graze");
         while (anim.GetBool("isGrazing"))
         {
             yield return null;
         }
-
+        //Rotate towards herd
         elapsedTime = 0f;
         waitTime = 10f;
         while (elapsedTime < waitTime)
@@ -108,6 +114,7 @@ public class llamaBehavior : MonoBehaviour
             //Debug.Log(elapsedTime + " " + waitTime);
             yield return null;
         }
+        //Move towards herd
         while (transform.position.x > currentPos.x && transform.position.z > currentPos.z)
         {
             if (!anim.GetBool("isWalking"))
@@ -119,6 +126,7 @@ public class llamaBehavior : MonoBehaviour
             transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) + 0.5f, transform.position.z);
             yield return null;
         }
+        //Get ready to do normal herd stuff again
         isDrinking = false;
         yield return null;
     }
